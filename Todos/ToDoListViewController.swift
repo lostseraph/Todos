a6ef00e2b20b1b3b8ac634dc0277af52bbd4b91f
Roscoe Sabale <rsabale43@gmail.com>
2018-03-04 01:07:46 -0500

    Add TableView with some hardcoded cells

---
+++ Todos/ToDoListViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  Todos
+//
+//  Created by Roscoe Sabale on 3/3/18.
+//  Copyright © 2018 LostSeRaph. All rights reserved.
+//
+
+import UIKit
+
+class ToDoListViewController: UITableViewController {
+    
+    let itemArray = ["Write Code", "Love Code", "Live Code"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+    //MARK - TableView Datasource Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+    
+    //MARK - TableView Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }
+        else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    
+    
+}
+
