e96ee812a48e2ccc19d257bf99bcb6c4f48bc934
Roscoe Sabale <rsabale43@gmail.com>
2018-03-03 23:42:49 -0500

    Initial commit

---
+++ Todos/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Todos
+//
+//  Created by Roscoe Sabale on 3/3/18.
+//  Copyright © 2018 LostSeRaph. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
