8dd68e4b334606563f76ac3620ae978a02646771
Roscoe Sabale <rsabale43@gmail.com>
2018-03-15 00:24:35 -0400

    Add load save data functionality

--- Todos/Controller/ToDoListViewController.swift
+++ Todos/Controller/ToDoListViewController.swift
@@ -17,23 +17,7 @@ class ToDoListViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-        
-        let newItem1 = Item()
-        newItem1.title = "Learn Code"
-        itemArray.append(newItem1)
-        
-        let newItem2 = Item()
-        newItem2.title = "Love Code"
-        itemArray.append(newItem2)
-        
-        let newItem3 = Item()
-        newItem3.title = "Live Code"
-        itemArray.append(newItem3)
-        
-//        if let items = defaults.array(forKey: "TodoListArray") as? [Item] {
-//            itemArray = items
-//        }
+        loadItems()
         
     }
 
@@ -92,6 +76,17 @@ class ToDoListViewController: UITableViewController {
             print("Error encoding item array, \(error)")
         }
         tableView.reloadData()
+    }
+    
+    func loadItems() {
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            let decoder = PropertyListDecoder()
+            do {
+                itemArray = try decoder.decode([Item].self, from: data)
+            } catch {
+                print("Error decoding item array, \(error)")
+            }
+        }
     }
 }
 
